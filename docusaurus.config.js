9c60bc85fba79c18e614f76ca6b5f866779e9eca
Paweł Kowaluk <pensjonatus@gmail.com>
2023-05-25 06:57:09 +0200

    add google and set POlish locale

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -28,8 +28,8 @@ const config = {
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
   i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
+    defaultLocale: "pl",
+    locales: ["pl"],
   },
 
   presets: [
@@ -45,6 +45,10 @@ const config = {
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
+        },
+        gtag: {
+          trackingID: "G-PZCSLJKETM",
+          anonymizeIP: true,
         },
       }),
     ],
