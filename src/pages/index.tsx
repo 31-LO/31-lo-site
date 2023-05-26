8456e4af62e7bded27749b917cca00704571f3af
Paweł Kowaluk <pensjonatus@gmail.com>
2023-05-26 21:25:54 +0200

    krakow frame

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,9 @@ function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
     <header className={clsx("hero hero--primary", styles.heroBanner)}>
+      <div className={styles.frame}>
+        <img alt="" src="/img/logo-krakow.png" className={styles.krakowLogo} />
+      </div>
       <div className="container">
         <div className="hero__title">
           <img
