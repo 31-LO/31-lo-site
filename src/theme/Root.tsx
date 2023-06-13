4ff6b1112f2dccde0c50898f570765075f549d0f
Paweł Kowaluk <pensjonatus@gmail.com>
2023-06-13 08:03:27 +0200

    enforce https on the client side

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,7 +1,13 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import '@fontsource/open-sans';
 
 // Default implementation, that you can customize
 export default function Root({ children }) {
+  useEffect(() => {
+    const { hostname, protocol, href } = window.location;
+    if (hostname !== 'localhost' && protocol !== 'http:') {
+      window.location.replace(href.replace('http:', 'https:'));
+    }
+  }, []);
   return <>{children}</>;
 }
