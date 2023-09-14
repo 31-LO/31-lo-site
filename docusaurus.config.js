6be21a1ab5bab3b5a4a1cb8beb7d5e9d972d9aa0
Pawel Kowaluk <pensjonatus@gmail.com>
2023-09-14 07:05:37 +0000

    napraw łączę

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -103,7 +103,7 @@ const config = {
               },
               {
                 label: 'Statut szkoły',
-                to: 'docs/statut-szkoły',
+                to: 'docs/statut',
               },
               {
                 label: 'Informacje dodatkowe',
