5d787de83de08828543ee0e8c93b429ad5ad863f
Pawel Kowaluk <pensjonatus@gmail.com>
2024-04-20 08:44:11 +0000

    add trailing slash

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -20,6 +20,7 @@ const config: Config = {
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
+  trailingSlash: true,
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
