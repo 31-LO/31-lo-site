689c518ec5f373b0475813cf43463ed55fae2d12
norbiros <norbiros@protonmail.com>
2025-07-27 21:38:33 +0200

    feat: add link to the discord server

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -175,6 +175,11 @@ const config: Config = {
             label: 'Facebook',
             position: 'right',
           },
+          {
+            to: 'https://discord.com/invite/5e2qXHWx',
+            label: 'Discord',
+            position: 'right',
+          },
         ],
       },
       footer: {
