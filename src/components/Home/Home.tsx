9c22ee6beb1718f504d462d87de8828ff444ea9a
Sanielia <me@sanielia.dev>
2026-09-15 09:53:48 +0200

    feat: Add parent-teacher meeting notice

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -4,6 +4,7 @@ import Layout from '@theme/Layout';
 import { JSX } from 'react';
 import styles from './Home.module.css';
 import HomepageHeader from './HomepageHeader';
+import NoticeBanner from '../NoticeBanner/NoticeBanner';
 
 export default function Home(): JSX.Element {
   return (
@@ -11,6 +12,7 @@ export default function Home(): JSX.Element {
       title={`Jedyne takie liceum 👋`}
       description="Liceum informatyczne o profilach programowanie i game dev, rekrutacja. Liceum Ogólnokształcące im. Romana Ingardena w Krakowie."
     >
+      <NoticeBanner text="📆 17 września zapraszamy na godzinę 17:00 na zebrania z rodzicami oraz na godzinę 18:00 na zebranie Rady Rodziców." />
       <HomepageHeader />
       <main className={styles.main}>
         <HomepageFeatures />
