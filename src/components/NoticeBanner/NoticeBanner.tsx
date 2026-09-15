301ed43cc96b717797d75687ad4753c11617c415
Sanielia <me@sanielia.dev>
2026-09-15 09:53:48 +0200

    feat: Add parent-teacher meeting notice

--- src/components/NoticeBanner/NoticeBanner.tsx
+++ src/components/NoticeBanner/NoticeBanner.tsx
@@ -1,14 +1,17 @@
 import Link from '@docusaurus/Link';
 import styles from './NoticeBanner.module.css';
 
-export default function NoticeBanner() {
+interface NoticeBannerProps {
+  text: string;
+  href?: string;
+}
+
+export default function NoticeBanner({ text, href }: NoticeBannerProps) {
   return (
     <div className={styles.noticeBanner}>
       <div className="container">
         <h2 className={styles.noticeText}>
-          <Link href="https://www.facebook.com/share/1D5fCGGk5G/">
-            🎉 Zapraszamy na dzień otwarty 25 kwietnia 2026!!!
-          </Link>
+          {href ? <Link href={href}>{text}</Link> : text}
         </h2>
       </div>
     </div>
